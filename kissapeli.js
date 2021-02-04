429fd5fa64639da428043615e3285d3c21b8e5cd
terho <terho.suonpaa@gmail.com>
2021-02-04 17:03:50 +0200

    projekti aloitettu

---
+++ kissapeli.js
@@ -0,0 +1,9 @@
+let taustakuva;
+let kissakuva;
+let taustan_korkeus = 400;
+let taustan_leveys = 800;
+
+function preload(){
+  taustakuva = loadImage("https://igno.cc/opetus/kuvat/tausta.png");
+  kissakuva = loadImage("https://igno.cc/opetus/kuvat/cat.png");
+}
